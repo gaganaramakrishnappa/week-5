821c6e4d9af9c4bc3c804c1af81668c5cee1a32d
gaganaramakrishnappa <gagana.ramakrishnappa@gmail.com>
2019-09-11 12:59:58 +0530

    first commit

---
+++ fact.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+	int i;
+	int j,num;
+	printf("enter the number\n");
+	scanf("%d",&num);
+	for(i=1;i<num;i++){
+		j=j*i;
+		printf("the factorial of %d is %d",num,j);
+		
+		}}
